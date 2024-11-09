cfd5587c169900e10286fdd9d276159f1572f6b4
Hendrix Tavarez <htavarez@gmail.com>
2024-11-09 16:35:47 -0500

    robots.txt and sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(eleventyConfig) {
 
   // copy assets/img to _site/img
   eleventyConfig.addPassthroughCopy({"src/assets/img": "img"});
+
+  // copy robots.txt and sitemap.xml to _site
+  eleventyConfig.addPassthroughCopy({"src/robots.txt": "robots.txt"});
+  eleventyConfig.addPassthroughCopy({"src/sitemap.xml": "sitemap.xml"});
+
   return {
     dir: {
       input: "src",
